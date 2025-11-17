25a68689b803e5d903f2aa9bf2a3cb5d7d41dc5d
Davi Lucas <davilucasde1@gmail.com>
2025-11-17 09:54:59 -0300

    Update sorts.h

--- src/sorts.h
+++ src/sorts.h
@@ -1,1 +1,16 @@
+#ifndef SORTS_H
+#define SORTS_H
+#include <stddef.h>
+
+typedef struct {
+    long long cmp;
+    long long swp;
+} Metrics;
+
+void reset_metrics(Metrics *m);
+void bubble_sort(int *v, size_t n, Metrics *m);
+void selection_sort(int *v, size_t n, Metrics *m);
+void insertion_sort(int *v, size_t n, Metrics *m);
+
+#endif
 
